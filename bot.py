cdadabc09cde7562f5b4c4c36bfc0693fc1aa54b
Nick Conklin <nconkl1@outlook.com>
2018-09-12 18:32:31 -0400

    Added $subnet-broadcast

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import discord
-import token
+import constants
 import ip_address
 import points
 
-TOKEN = token.TOKEN
+TOKEN = constants.TOKEN
 
 client = discord.Client()
 
@@ -18,25 +18,104 @@ async def on_message(message):
 
     # Shutdown
     if message.content.startswith('$shutdown'):
-        client.close()
+        await client.send_message(message.channel, ':wave: Bye :wave: bye!')
+        await client.close()
+
+    if message.content.startswith('$restart'):
+        await client.send_message(message.channel, 'Okay brb!')
+        await client.close()
+        await client.connect()
 
     # Guessing game for guessing the network address of an IP
+    # $subnet-network
     if message.content.startswith('$subnet-network'):
-        question = 'What is the Network address of {}'.format(ip_address.formatted_ip_address)
+        question = 'What is the Network address of **{}**?'.format(ip_address.formatted_ip_address)
         answer = ip_address.ip_network
-        await client.send_message(message.channel, question)
+        embed_question = discord.Embed(
+            title="Subnetting: Finding Network Addresses",
+            description=question,
+            color=0x00FF00  # Green
+        )
 
-        guess = await client.wait_for_message(timeout=25.0, author=author)
+        embed_timeout = discord.Embed(
+            title="Subnetting: Finding Network Addresses",
+            description='Sorry {}, you took too long. It was **{}**'.format(author.mention, answer),
+            color=0xFF0000  # Red
+        )
 
+        embed_correct = discord.Embed(
+            title="Subnetting: Finding Network Addresses",
+            description='Correct {}!  +10 Points!'.format(author.mention),
+            color=0x00FF00  # Green
+        )
+
+        embed_wrong = discord.Embed(
+            title="Subnetting: Finding Network Addresses",
+            description='Sorry {}. It is actually **{}**. -5 Points'.format(author.mention, answer),
+            color=0xFF0000
+        )
+
+        await client.send_message(message.channel, embed=embed_question)
+
+        guess = await client.wait_for_message(timeout=30.0, author=author)
+
+        # If the user enters nothing
         if guess is None:
-            fmt = 'Sorry {}, you took too long. It was {}.'
-            await client.send_message(message.channel, fmt.format(author.mention, answer))
+            await client.send_message(message.channel, embed=embed_timeout)
             return
+        # Correct answer given
         if guess == answer:
-            await client.send_message(message.channel, 'Correct {}!  +10 Points!'.format(author.mention))
+            await client.send_message(message.channel, embed=embed_correct)
             points.set_points(user_id, points.get_points(user_id) + 10)
+        # Wrong answer given
         else:
-            await client.send_message(message.channel, 'Sorry. It is actually {}. -5 Points'.format(answer))
+            await client.send_message(message.channel, embed=embed_wrong)
+            points.set_points(user_id, points.get_points(user_id) - 5)
+
+    # Guessing game for guessing the broadcast address of an IP
+    # $subnet-broadcast
+    if message.content.startswith('$subnet-broadcast'):
+        question = 'What is the Broadcast address of **{}**?'.format(ip_address.formatted_ip_address)
+        answer = ip_address.ip_network.broadcast_address
+        embed_question = discord.Embed(
+            title="Subnetting: Finding Broadcast Addresses",
+            description=question,
+            color=0x00FF00  # Green
+        )
+
+        embed_timeout = discord.Embed(
+            title="Subnetting: Finding Broadcast Addresses",
+            description='Sorry {}, you took too long. It was **{}**'.format(author.mention, answer),
+            color=0xFF0000  # Red
+        )
+
+        embed_correct = discord.Embed(
+            title="Subnetting: Finding Broadcast Addresses",
+            description='Correct {}!  +10 Points!'.format(author.mention),
+            color=0x00FF00  # Green
+        )
+
+        embed_wrong = discord.Embed(
+            title="Subnetting: Finding Broadcast Addresses",
+            description='Sorry {}. It is actually **{}**. -5 Points'.format(author.mention, answer),
+            color=0xFF0000  # Red
+        )
+
+        await client.send_message(message.channel, embed=embed_question)
+
+        guess = await client.wait_for_message(timeout=30.0, author=author)
+
+        # If the user enters nothing
+        if guess is None:
+            await client.send_message(message.channel, embed=embed_timeout)
+            return
+        # Correct answer given
+        if guess == answer:
+            await client.send_message(message.channel, embed=embed_correct)
+            points.set_points(user_id, points.get_points(user_id) + 10)
+        # Wrong answer given
+        else:
+            await client.send_message(message.channel, embed=embed_wrong)
             points.set_points(user_id, points.get_points(user_id) - 5)
 
     # Hello (Test)
