39d11175dc0ff9fced6b7ea74b9effbb56aa1da5
Nick Conklin <nconkl1@outlook.com>
2018-09-12 13:37:23 -0400

    Added $subnet-network command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import token
+import ip_address
 
 TOKEN = token.TOKEN
 
@@ -15,6 +16,24 @@ async def on_message(message):
     # Shutdown
     if message.content.startswith('$shutdown'):
         client.close()
+
+    # Guessing game for guessing the network address of an IP
+    if message.content.startswith('$subnet-network'):
+        question = 'What is the Network address of {}'.format(ip_address.formatted_ip_address)
+        answer = ip_address.ip_network
+        await client.send_message(message.channel, question)
+
+        guess = await client.wait_for_message(timeout=25.0, author=message.author)
+
+        if guess is None:
+            fmt = 'Sorry, you took too long. It was {}.'
+            await client.send_message(message.channel, fmt.format(answer))
+            return
+        if guess == answer:
+            # TODO: Implement point system
+            await client.send_message(message.channel, 'Correct {}!  +10 Points!'.format(message.author))
+        else:
+            await client.send_message(message.channel, 'Sorry. It is actually {}. -5 Points'.format(answer))
 
     # Hello (Test)
     if message.content.startswith('$hello'):
