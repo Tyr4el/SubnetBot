ebc54494bfddf48fcbec29c4baff2e30f1979fe5
Nick Conklin <nconkl1@outlook.com>
2018-09-12 16:00:40 -0400

    Added points.py

---
+++ points.py
@@ -0,0 +1,26 @@
+import json
+
+data = {}
+
+
+def load():
+    with open('points.json', 'r') as file:
+        json.load(file)
+    print('Opened points.json file')
+
+
+def save():
+    with open('points.json', 'w') as file:
+        json.dump(data, file)
+    print('Saved points to file points.json.')
+
+
+def get_points(user_id):
+    if user_id not in data:
+        return 0
+    else:
+        return data[user_id]
+
+
+def set_points(user_id, points):
+    data[user_id] = points
